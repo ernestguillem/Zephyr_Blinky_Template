9964b6a0a06e3a2f0453f652a2ae47219e9e163c
Ernest Guillem <ernest@guillem.org>
2025-05-27 20:59:05 +0200

    Simplified example with no main thread and 2 blinking tasks

--- app1/src/main.c
+++ app1/src/main.c
@@ -6,7 +6,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/random/random.h>
+
 #include "usr_fun.h"
 
 /* 1000 msec = 1 sec */
@@ -17,6 +17,8 @@
 #define LED1_NODE DT_ALIAS(led1)
 #define LED2_NODE DT_ALIAS(led2)
 
+#define STACKSIZE	500
+
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
@@ -25,38 +27,42 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 
-int main(void)
-{
-	bool led_state = true;
-	uint32_t rnd = 0;
 
-	if (!gpio_is_ready_dt(&led0) && !gpio_is_ready_dt(&led1) && !gpio_is_ready_dt(&led2) ) {
-		return 0;
+
+void blink0(void)
+{
+	gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+	while(1){
+		gpio_pin_toggle_dt(&led0);
+		k_msleep(200);
 	}
 
-	if(gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE) < 0){
-		return 0;
-	};
-	if(gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE) < 0){
-		return 0;
-	};
-	if(gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE) < 0){
-		return 0;
-	};
+}
+
+void blink1(void)
+{
+	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+
+	while (1){
+		gpio_pin_toggle_dt(&led1);
+		k_msleep(400);
+	}
+		
+
+}
+
+int main(void)
+{	
+	gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+
 	gpio_pin_set_dt(&led2, true);
 	
 	#ifdef CONFIG_USR_FUN
     usr_fun();
 	#endif
-
-	while (1) {
-		rnd = sys_rand32_get();
-		gpio_pin_toggle_dt(&led0);
-		gpio_pin_toggle_dt(&led1);
 	
-		led_state = !led_state;
-		printk("LED state: %s - Random: %u\n", led_state ? "ON" : "OFF", rnd);
-		k_msleep(SLEEP_TIME_MS);
-	}
 	return 0;
 }
+
+K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL, 7, 0, 0);
