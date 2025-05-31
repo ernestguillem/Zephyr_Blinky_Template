c3f6daaadaa97ac622cf10e71350a83cf245570d
Ernest Guillem <ernest@guillem.org>
2025-05-31 15:59:13 +0200

    Style refactor

--- app1/src/main.c
+++ app1/src/main.c
@@ -1,12 +1,10 @@
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/logging/log.h>
-#include "zephyr/drivers/pwm.h"
 #include "zephyr/device.h"
+#include "zephyr/drivers/pwm.h"
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
 #include "usr_fun.h"
-
-
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
@@ -15,9 +13,9 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #define SLEEP_TIME_LED1_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE   DT_ALIAS(led0)
-#define LED1_NODE   DT_ALIAS(led1)
-#define LED2_NODE   DT_ALIAS(led2)
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
 #define BUTTON_NODE DT_ALIAS(usrbtn)
 
 #define STACKSIZE 500
@@ -32,46 +30,45 @@ static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
 static struct gpio_callback button_cb_data;
 
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-	printk("Button pressed\n");
-	gpio_pin_toggle_dt(&led2);
+const struct pwm_dt_spec pwm_spec = PWM_DT_SPEC_GET(DT_ALIAS(mypwmalias));
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+                    uint32_t pins) {
+  printk("Button pressed\n");
+  gpio_pin_toggle_dt(&led2);
 }
 
-_Noreturn void blink0(void)
-{
-	gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-	while (1) {
-		gpio_pin_toggle_dt(&led0);
-		k_msleep(SLEEP_TIME_LED0_MS);
-	}
+_Noreturn void blink0(void) {
+  gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+  while (1) {
+    gpio_pin_toggle_dt(&led0);
+    k_msleep(SLEEP_TIME_LED0_MS);
+  }
 }
 
-_Noreturn void blink1(void)
-{
-	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+_Noreturn void blink1(void) {
+  gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
 
-	while (1) {
-		gpio_pin_toggle_dt(&led1);
-		k_msleep(SLEEP_TIME_LED1_MS);
-	}
+  while (1) {
+    gpio_pin_toggle_dt(&led1);
+    k_msleep(SLEEP_TIME_LED1_MS);
+  }
 }
 
-int main(void)
-{
-	gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_set_dt(&led2, false);
+int main(void) {
+  gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+  gpio_pin_set_dt(&led2, false);
 
-	gpio_pin_configure_dt(&button, GPIO_INPUT);
-	gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
+  gpio_pin_configure_dt(&button, GPIO_INPUT);
+  gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+  gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+  gpio_add_callback(button.port, &button_cb_data);
 
 #ifdef CONFIG_USR_FUN
-	usr_fun();
+  usr_fun();
 #endif
 
-	return 0;
+  return 0;
 }
 
 K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL, 7, 0, 0);
--- app1/src/shell.c
+++ app1/src/shell.c
@@ -1,50 +1,43 @@
-#include <zephyr/kernel.h>
-#include "usr_fun.h"
-#include <stdlib.h>
-#include <zephyr/shell/shell.h>
-#include <zephyr/logging/log.h>
 #include "zephyr/drivers/pwm.h"
-
+#include <zephyr/shell/shell.h>
+#include <stdlib.h>
+#include "usr_fun.h"
 
 LOG_MODULE_REGISTER(shell, LOG_LEVEL_DBG);
 
-static const struct pwm_dt_spec pwm_spec = PWM_DT_SPEC_GET(DT_ALIAS(mypwmalias));
+extern const struct pwm_dt_spec pwm_spec;
 
-static int function1(const struct shell *sh, size_t argc, char **argv)
-{
-        ARG_UNUSED(argc);
-        ARG_UNUSED(argv);
-        shell_print(sh, "pong");
-        return 0;
+static int function1(const struct shell *sh, size_t argc, char **argv) {
+  ARG_UNUSED(argc);
+  ARG_UNUSED(argv);
+  shell_print(sh, "pong");
+  return 0;
 }
 
-static int function2(const struct shell *sh, size_t argc, char **argv)
-{
-		ARG_UNUSED(argc);
-        ARG_UNUSED(argv);
-		ARG_UNUSED(sh);
-        
-        uint8_t dump[] = "This is a very stupid example demo dump";
-        
-        LOG_ERR("Error message has level  : %d", LOG_LEVEL_ERR);
-	    LOG_WRN("Warning message has level: %d", LOG_LEVEL_WRN);
-	    LOG_INF("Info message has level   : %d", LOG_LEVEL_INF);
-	    LOG_DBG("Debug message has level  : %d and it always include the function name", LOG_LEVEL_DBG);
-	    LOG_HEXDUMP_DBG(&dump, sizeof(dump), "Example dump = ");
-        return 0;
+static int function2(const struct shell *sh, size_t argc, char **argv) {
+  ARG_UNUSED(argc);
+  ARG_UNUSED(argv);
+  ARG_UNUSED(sh);
+
+  uint8_t dump[] = "This is a very stupid example demo dump";
+
+  LOG_ERR("Error message has level  : %d", LOG_LEVEL_ERR);
+  LOG_WRN("Warning message has level: %d", LOG_LEVEL_WRN);
+  LOG_INF("Info message has level   : %d", LOG_LEVEL_INF);
+  LOG_DBG("Debug message has level  : %d and it always include the function name", LOG_LEVEL_DBG);
+  LOG_HEXDUMP_DBG(&dump, sizeof(dump), "Example dump = ");
+  return 0;
 }
 
-static int function3(const struct shell *sh, size_t argc, char **argv)
-{		
-		uint8_t counter = atoi(argv[1]);
-        for (size_t i = 0; i < counter; i++){
-			shell_print(sh, "Love zephyr");
-		}
-        return 0;
+static int function3(const struct shell *sh, size_t argc, char **argv) {
+  uint8_t counter = atoi(argv[1]);
+  for (size_t i = 0; i < counter; i++) {
+    shell_print(sh, "Love zephyr");
+  }
+  return 0;
 }
 
-static int function4(const struct shell *sh, size_t argc, char **argv)
-{
+static int function4(const struct shell *sh, size_t argc, char **argv) {
   uint16_t pulse = atoi(argv[1]);
 
   pwm_set_dt(&pwm_spec, PWM_MSEC(1000), PWM_MSEC(pulse));
@@ -52,12 +45,12 @@ static int function4(const struct shell *sh, size_t argc, char **argv)
   return 0;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
-	SHELL_CMD(demo1, NULL, "First demo function", function1),
-	SHELL_CMD(demo2, NULL, "Second demo function", function2),
-	SHELL_CMD_ARG(demo3, NULL, "Third demo function", function3, 2, 0),
-	SHELL_CMD_ARG(pwm, NULL, "Pwm demo function", function4, 2, 0),
-	SHELL_SUBCMD_SET_END /* Array terminated. */
+SHELL_STATIC_SUBCMD_SET_CREATE(
+    sub_demo, SHELL_CMD(demo1, NULL, "First demo function", function1),
+    SHELL_CMD(demo2, NULL, "Second demo function", function2),
+    SHELL_CMD_ARG(demo3, NULL, "Third demo function", function3, 2, 0),
+    SHELL_CMD_ARG(pwm, NULL, "Pwm demo function", function4, 2, 0),
+    SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 
 SHELL_CMD_REGISTER(demo, &sub_demo, "Log test", NULL);
